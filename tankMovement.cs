34649e669b58c0c2c9c45a223ef7129dcfcc7365
npbehunin <npbehunin@gmail.com>
2016-12-05 20:13:53 -0700

    tankMovement.cs
    Updated with count display on canvas.

--- tankMovement.cs
+++ tankMovement.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class tankMovement : MonoBehaviour {
-	private int score = 0;
+	private int count;
 
 	public float moveSpeed;
 	public float turnSpeed;
+	public Text countText;
 
 	// Use this for initialization
 	void Start () {
-	
+		count = 0;
+		SetCountText ();
 	}
 	
 	// Update is called once per frame
@@ -21,4 +24,18 @@ public class tankMovement : MonoBehaviour {
 		transform.Rotate(0, x, 0);
 		transform.Translate(0, 0, z);
 	}
+	// Collision detect with coin and set coin as inactive.
+  	void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ("Coin"))
+        {
+            other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+        }
+    }
+	void SetCountText () {
+			countText.text = "Count: " + count.ToString ();
+	}
 }
+
